5a20ace8fa45f0884eee816a3ebbefc53d25436f
mohammedHatemm <mohamed.hatem97825@gmail.com>
2025-05-25 07:55:54 +0300

    type in oop

--- oop/index.php
+++ oop/index.php
@@ -1,10 +1,69 @@
 <?php
 // inhertance
-class Student
+// class Student
+// {
+//   public string $name = "moahemd";
+//   public $age;
+//   protected $hamdaa = "this from hamada ";
+//   private $birthdate = "25/8/1997";
+//   public function printBirthDate()
+//   {
+//     return $this->birthdate;
+//   }
+
+//   // public function printHamda()
+//   // {
+//   //   return $this->hamdaa;
+//   // }
+// }
+// class Std2 extends Student
+// {
+
+//   public function printHamda()
+//   {
+//     return $this->hamdaa;
+//   }
+// }
+// $student = new Std2();
+// echo $student->name;
+// echo $student->printHamda();
+// echo "<br>";
+// echo $student->printBirthDate();
+
+// public -> اي حد ممكن يشوفوا او يستخدمه
+//protected -> ممكن تستخدمه داخل ال calss بس او ال calss الي وارث منه
+// private -> بيتم استخدامه مع نقس ال class فقط
+
+//لو عاوز اطبع  ال private لاوم اكون ال function الي هتبظبعا public في ال class بتاععا
+
+
+class Factory
 {
-  public string $name = "moahemd";
-  public $age;
+  public $shap = "shap from Factory ";
+  protected $engin = "engin from factory ";
+  private $draw = "draw from factory";
+  public function  print_draw()
+  {
+    return $this->draw;
+  }
 }
-class Std2 extends Student {}
-$student = new Std2();
-echo $student->name;
+
+class Car extends Factory
+{
+  public function print_shap()
+  {
+    return $this->shap;
+  }
+  public function print_engin()
+  {
+    return $this->engin;
+  }
+}
+
+$car1 = new Car;
+
+echo $car1->print_shap();
+echo "<br>";
+echo $car1->print_engin();
+echo " <br>";
+echo $car1->print_draw();
